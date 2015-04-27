c16b0de57fbd9acd02c6721565208716ea5942f5
Bassam Alfarhan <alfarhan@csc.kth.se>
2015-04-27 12:53:03 +0200

    not much done, just a sync

--- koolc/src/main/scala/koolc/code/CodeGeneration.scala
+++ koolc/src/main/scala/koolc/code/CodeGeneration.scala
@@ -5,7 +5,7 @@ import ast.Trees._
 import analyzer.Symbols._
 import analyzer.Types._
 import cafebabe._
-import AbstractByteCodes.{New => _, _}
+import AbstractByteCodes.{ New => _, _ }
 import ByteCodes._
 import utils._
 
@@ -17,7 +17,28 @@ object CodeGeneration extends Pipeline[Program, Unit] {
     /** Writes the proper .class file in a given directory. An empty string for dir is equivalent to "./". */
     def generateClassFile(sourceName: String, ct: ClassDecl, dir: String): Unit = {
       // TODO: Create code handler, save to files ...
-      ???
+
+      val classFile = new cafebabe.ClassFile(ct.id.value, if (ct.parent.isEmpty) None else Some(ct.parent.get.value))
+      classFile.setSourceFile(sourceName)
+
+      
+      
+      for(m <- ct.methods){
+        var loa = List[String]()
+        m.args.foreach(a => loa = loa :+ a.id.value)
+        val mh = classFile.addMethod(m.retType.getType.toString(), m.id.value, loa)
+        generateMethodCode(mh.codeHandler, m)
+      }
+
+      //      //DO SOME STUFF
+      //      codeHandler <<
+      //        GetStatic("java/lang/System", "out", "Ljava/io/PrintStream;") <<
+      //        Ldc("Hello world!") <<
+      //        InvokeVirtual("java/io/PrintStream", "println", "(Ljava/lang/String;)V") <<
+      //        RETURN
+      //      //STOP DOING STUFF
+
+      classFile.writeToFile("./classfiles/" + ct.id.value + ".class")
     }
 
     // a mapping from variable symbols to positions in the local variables
@@ -36,7 +57,60 @@ object CodeGeneration extends Pipeline[Program, Unit] {
       ch.freeze
     }
 
-    val outDir = ctx.outDir.map(_.getPath+"/").getOrElse("./")
+    def codeGenerationFuncs(ch: cafebabe.CodeHandler, node: Tree) = {
+      node match {
+        case IntArrayType() =>
+        case IntType() =>
+        case BooleanType() =>
+        case StringType() =>
+
+        case Block(stats: List[StatTree]) =>
+        case If(expr: ExprTree, thn: StatTree, els: Option[StatTree]) =>
+        case While(expr: ExprTree, stat: StatTree) =>
+        case Println(expr: ExprTree) => {
+          ch <<
+            GetStatic("java/lang/System", "out", "Ljava/io/PrintStream;") <<
+            Ldc("HW") <<
+            InvokeVirtual("java/io/PrintStream", "println", "(Ljava/lang/String;)V") <<
+            RETURN
+        }
+        case Assign(id: Identifier, expr: ExprTree) =>
+        case ArrayAssign(id: Identifier, index: ExprTree, expr: ExprTree) =>
+
+        case And(lhs: ExprTree, rhs: ExprTree) => {
+          
+        }
+        case Or(lhs: ExprTree, rhs: ExprTree) =>
+        case Plus(lhs: ExprTree, rhs: ExprTree) => {
+          
+        }
+        case Minus(lhs: ExprTree, rhs: ExprTree) => {
+          
+          ch << ILoad(1) << ALoad(0) <<
+               ILoad(1) << Ldc(1) << ISUB
+        }
+        case Times(lhs: ExprTree, rhs: ExprTree) =>
+        case Div(lhs: ExprTree, rhs: ExprTree) =>
+        case LessThan(lhs: ExprTree, rhs: ExprTree) =>
+        case Equals(lhs: ExprTree, rhs: ExprTree) =>
+        case ArrayRead(arr: ExprTree, index: ExprTree) =>
+        case ArrayLength(arr: ExprTree) =>
+        case MethodCall(obj: ExprTree, meth: Identifier, args: List[ExprTree]) =>
+        case IntLit(value: Int) =>
+        case StringLit(value: String) =>
+
+        case True() =>
+        case False() =>
+        case Identifier(value: String) =>
+        case This() =>
+        case NewIntArray(size: ExprTree) =>
+        case New(tpe: Identifier) =>
+        case Not(expr: ExprTree) =>
+
+      }
+    }
+
+    val outDir = ctx.outDir.map(_.getPath + "/").getOrElse("./")
 
     val f = new java.io.File(outDir)
     if (!f.exists()) {
@@ -52,6 +126,11 @@ object CodeGeneration extends Pipeline[Program, Unit] {
 
     // Now do the main method
     // ...
+//  TODO    val codeHandler = classFile.addMainMethod.codeHandler
+//      codeHandler.freeze
+
+      
+
   }
 
 }
